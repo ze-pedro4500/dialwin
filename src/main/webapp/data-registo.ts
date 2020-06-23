c1325700d687ef0bdbc15c5d18ae260763424529
ze-pedro4500 <64307625+ze-pedro4500@users.noreply.github.com>
2020-06-23 15:16:28 +0100

    Delete data-registo.ts

--- src/main/webapp/data-registo.ts
+++
@@ -1,6 +0,0 @@
-import { IRegistoData } from './app/shared/model/registo-data.model';
-import { IDoenteRegistosIntervencoes } from './app/shared/model/doente-registos-intervencoes.model'
-export class DataRegisto {
-    data: IRegistoData;
-    reg: IDoenteRegistosIntervencoes;
-}
